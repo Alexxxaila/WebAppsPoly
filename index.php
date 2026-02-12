ed2c17fee4578032c1638565e1b0b88fa708cc49
Alexxxaila <alexandra.ilyina2004@yandex.ru>
2026-02-12 06:47:22 +0300

    Add main page

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="robots" content="noindex">
+	<title>ILYINA-WEB-APP</title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Kalam:wght@300;400;700&family=Reddit+Sans:ital,wght@0,200..900;1,200..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet">
+
+	<link rel="stylesheet" href="course.css">
+</head>
+
+<body>
+
+	<header>
+		<h1><a href="https://github.com/Alexxxaila/ilyina-web-dev-2025-1" target="_blank">ILYINA-WEB-APPS</a></h1>
+		<div class="task">
+			<a class='task_link' href='./Lab1_20k_leagues_php/index.php' target="_blank">Лабораторная работа A-1</a>
+			<!--<a class='task_link' href='./lab2/index.html'>Лабораторная работа 2</a>
+			<a class="task_link" href="./lab3/index.html">Лабораторная работа 3</a>
+			<a class="task_link" href="./lab4/index.html">Лабораторная работа 4</a>
+			<a class="task_link" href="./lab5/index.html">Лабораторная работа 5</a>
+			<a class="task_link" href="./lab6/index.html">Лабораторная работа 6</a>
+			<a class="task_link" href="./lab7/index.html">Лабораторная работа 7</a>
+			<a class="task_link" href="./lab8/index.html">Лабораторная работа 8</a>
+			<a class="task_link" href="./lab9/index.html">Лабораторная работа 9</a>
+			<a class="task_link" href="./lab10/landing.html">Лабораторная работа 10</a>
+			<a class='task_link' href='./add-task1-2/add-task1-2.html'>Дополнительные задания 1-2</a>-->
+		</div>
+	</header>
+
+</body>
+
+</html>
