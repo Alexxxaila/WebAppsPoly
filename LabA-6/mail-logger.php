4b362d27caf9e3f6c37ea16ae40ba81cf16f887c
Alexxxaila <alexandra.ilyina2004@yandex.ru>
2026-04-02 15:16:20 +0300

    add logger

---
+++ LabA-6/mail-logger.php
@@ -0,0 +1,9 @@
+#!/usr/bin/env php
+<?php
+$email = file_get_contents('php://stdin');
+$logDir = __DIR__ . '/mail_logs';
+if (!is_dir($logDir)) mkdir($logDir, 0777, true);
+$filename = $logDir . '/' . date('Y-m-d_H-i-s') . '_' . uniqid() . '.eml';
+file_put_contents($filename, $email);
+exit(0);
+?>
